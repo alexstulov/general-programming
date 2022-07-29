0ae4fc21dc5ec457eb3579320b5de3803cd98dd9
Alex Stulov <a.stulov.web@gmail.com>
2022-07-29 10:32:06 +0300

    Ignore node_modules

--- index.js
+++
@@ -1,7 +0,0 @@
-const pigItTest = require ('./tests/pigItTest');
-const numberToEnglishTest = require ('./tests/numberToEnglishTest');
-const hexStringToRGBTest = require ('./tests/hexStringToRGBTest');
-
-// pigItTest();
-numberToEnglishTest();
-// hexStringToRGBTest();
