9e8a64bf638d402423d85a6cfec41d8fc280a9c6
Alex Stulov <a.stulov.web@gmail.com>
2023-01-17 16:10:54 +0300

    Create concepts folder. Add multiplePointers concept

---
+++ helpers/concepts/multiplePointers.js
@@ -0,0 +1,29 @@
+function sumZero (arr) { // O(N)
+  let left = 0
+  let right = arr.length - 1
+  while (left < right) {
+    const sum = arr[left] + arr[right]
+    if (sum === 0) {
+      return [arr[left], arr[right]]
+    } else if (sum > 0) {
+      right--
+    } else {
+      left++
+    }
+  }
+}
+
+// function sumZero (arr) { // O(N^2)
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       if (arr[i] + arr[j] === 0) {
+//         return [arr[i], arr[j]]
+//       }
+//     }
+//   }
+//   return null
+// }
+
+console.log(sumZero([-3, -2, -1, 0, 1, 2, 3]), [-3, 3])
+console.log(sumZero([-2, 0, 1, 3]), null)
+console.log(sumZero([1, 2, 3]), null)
